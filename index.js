e5549d586646d417a1fc35d7c53a651e50e84524
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-08-03 22:28:27 +0100

    Move call to addJobs() to end of file
    Each run gives slightly different output with not all messages being seen. It just seems to be a timing thing. But you would expect all the setup to have happened (queues, workers etc) before the jobs are created and added to the queues.
    
    So, we move the call to addJobs() to the end of the file.

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ async function addJobs () {
   await myQueue.add('myJobName', { qux: 'baz' })
 }
 
-await addJobs()
-
 const worker = new Worker('foo', async job => {
   // Will print { foo: 'bar' } for the first job and { qux: 'baz' } for the second. We pass our strings seperately
   // rather than formatting them as one to avoid the output being '2 [object Object]'
@@ -42,3 +40,5 @@ queueEvents.on('completed', ({ jobId, returnValue }) => {
 queueEvents.on('failed', ({ jobId, failedReason }) => {
   console.log(`${jobId} has failed with reason ${failedReason}`)
 })
+
+await addJobs()
